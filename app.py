f530f23cd703a0d9c936364b5f6c73a8fc46669f
edoruin <edwinjeremiasagustinyack@gmail.com>
2025-12-03 00:03:00 -0400

    agregando datos de tablas de ranking en app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import pandas as pd
 import numpy as np 
 from datetime import date
 import ETL #Modulo de extraccion de los datos de la API
-
+import altair as alt
 
 # --- 1. CONFIGURACIÓN DE LA PÁGINA ---
 st.set_page_config(
@@ -24,15 +24,16 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+st.title("Bienvenido a LIDOM Stats Dashboard")
+st.markdown('¿Quien crees que ganara?')
 
-
-st.title("Obtener Juegos por Año")
+st.header("Obtener Juegos por Año")
 st.markdown("Introduce el año que quieres analizar")
 
-# Argumento de juegos, del usuario
 
+# Argumento de juegos, del usuario
 año_seleccionado = st.number_input(
-    "Año de los juegos a buscar (Introduce 0 para buscar todos):",
+    "Año de los juegos a buscar",
     min_value=0,
     max_value=3000,
     value=0, # Por defecto busca todos
@@ -58,5 +59,15 @@ if st.button("Buscar"): # Logica para añadir el resultado que usuario introduzc
 
     if not juegos.empty: #si existe informacion en el año solicitado
         st.dataframe(juegos, use_container_width=True)
+        st.header('RANKINGS')
+        st.markdown("---")
+        st.header("TOP hits home")        
+        hits_ranking_home = juegos['scores.home.hits'].groupby(juegos['teams.home.name']).sum().sort_values(ascending=False)
+        st.dataframe(hits_ranking_home)
+        st.header("TOP hits away")        
+        hits_ranking_away = juegos['scores.away.hits'].groupby(juegos['teams.away.name']).sum().sort_values(ascending=False)
+        st.dataframe(hits_ranking_away)
+        
     else:
         st.warning(f"No se encontraron juegos para el año {año_seleccionado}.")
+
