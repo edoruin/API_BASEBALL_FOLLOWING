e23ed701b144880ffef33cb56130b8ad0a66e68b
edoruin <edwinjeremiasagustinyack@gmail.com>
2025-12-03 00:20:51 -0400

    Agregando RANKINGs de run, hits and errors en app.py

--- app.py
+++ app.py
@@ -58,15 +58,40 @@ if st.button("Buscar"): # Logica para añadir el resultado que usuario introduzc
     st.success("¡Petición completada!")
 
     if not juegos.empty: #si existe informacion en el año solicitado
+
         st.dataframe(juegos, use_container_width=True)
         st.header('RANKINGS')
         st.markdown("---")
-        st.header("TOP hits home")        
-        hits_ranking_home = juegos['scores.home.hits'].groupby(juegos['teams.home.name']).sum().sort_values(ascending=False)
-        st.dataframe(hits_ranking_home)
-        st.header("TOP hits away")        
-        hits_ranking_away = juegos['scores.away.hits'].groupby(juegos['teams.away.name']).sum().sort_values(ascending=False)
-        st.dataframe(hits_ranking_away)
+        col1, col2 = st.columns(2) # creando dos columnas
+        with col1: 
+            st.header("Bateos en casa")        
+            hits_ranking_home = juegos['scores.home.hits'].groupby(juegos['teams.home.name']).sum().sort_values(ascending=False)
+            st.dataframe(hits_ranking_home, height = 250)
+
+            st.header("Carreras de casa")        
+            Runs_ranking_home = juegos['scores.home.total'].groupby(juegos['teams.home.name']).sum().sort_values(ascending=False)
+            st.dataframe(Runs_ranking_home, height = 250)
+
+            st.header("Errores en casa")        
+            Runs_ranking_home = juegos['scores.home.errors'].groupby(juegos['teams.home.name']).sum().sort_values(ascending=False)
+            st.dataframe(Runs_ranking_home, height = 250)
+
+
+        with col2:
+
+            st.header("Bateos en visitante")        
+            hits_ranking_away = juegos['scores.away.hits'].groupby(juegos['teams.away.name']).sum().sort_values(ascending=False)
+            st.dataframe(hits_ranking_away, height = 250)
+
+            st.header("Carreras de visitante")        
+            Runs_ranking_home = juegos['scores.away.total'].groupby(juegos['teams.away.name']).sum().sort_values(ascending=False)
+            st.dataframe(Runs_ranking_home, height = 250)
+
+            st.header("Errores de visitante")        
+            Runs_ranking_home = juegos['scores.away.errors'].groupby(juegos['teams.away.name']).sum().sort_values(ascending=False)
+            st.dataframe(Runs_ranking_home, height = 250)
+
+        
         
     else:
         st.warning(f"No se encontraron juegos para el año {año_seleccionado}.")
